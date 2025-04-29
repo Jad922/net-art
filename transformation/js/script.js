fa1594960b6fe9235bc13a4b06cd0ff64ecd61ff
Jad922 <150983019+Jad922@users.noreply.github.com>
2025-04-28 23:34:19 -0400

    Add files via upload

---
+++ transformation/js/script.js
@@ -0,0 +1,71 @@
+$(document).ready(function(){
+  $(".monkey").click(function(){
+    $("img").height(600);
+    //$("item1").animate({top: '-50%'});
+  });
+});
+
+
+
+$(document).ready(function(){
+    $(".button1").click(function(){
+      $(".button1").animate({left: '-100%'});
+      //$("item1").animate({top: '-50%'});
+    });
+  });
+
+$(document).ready(function(){
+    $(".button2").click(function(){
+      $(".button2").animate({top: '-100%'});
+      //$("item1").animate({top: '-50%'});
+    });
+  });
+
+$(document).ready(function(){
+    $(".button3").click(function(){
+      $(".button3").animate({right: '-100%'});
+      //$("item1").animate({top: '-50%'});
+    });
+  });
+
+$(document).ready(function(){
+    $(".button4").click(function(){
+      $(".button4").animate({left: '-100%'});
+      //$("item1").animate({top: '-50%'});
+    });
+  });
+
+$(document).ready(function(){
+    $(".button5").click(function(){
+      $(".button5").animate({top: '-200%'});
+      //$("item1").animate({top: '-50%'});
+    });
+  });
+
+$(document).ready(function(){
+    $(".button6").click(function(){
+      $(".button6").animate({right: '-100%'});
+      //$("item1").animate({top: '-50%'});
+    });
+  });
+
+$(document).ready(function(){
+    $(".button7").click(function(){
+      $(".button7").animate({left: '-100%'});
+      //$("item1").animate({top: '-50%'});
+    });
+  });
+
+$(document).ready(function(){
+    $(".button8").click(function(){
+      $(".button8").animate({top: '-300%'});
+      //$("item1").animate({top: '-50%'});
+    });
+  });
+
+$(document).ready(function(){
+    $(".button9").click(function(){
+      $(".button9").animate({right: '-100%'});
+      //$("item1").animate({top: '-50%'});
+    });
+  });
